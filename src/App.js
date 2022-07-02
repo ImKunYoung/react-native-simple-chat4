e1e3ed5f6e51f3896b4c1b8697a7e309ec8603a6
ImKunYoung <lim8667@gmail.com>
2022-07-02 11:54:59 +0900

    앱 아이콘, 로딩 화면 추가 // appLoading -> 업데이트 됨 / expo-splash-screen 라이브러리의 SplashScreen로 대체할 것

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {ThemeProvider} from "styled-components";
-import {theme} from "./theme";
 import {StatusBar} from "react-native";
+import {Asset as Font, Asset} from "expo-asset";
+import {theme} from "./theme";
+import * as SplashScreen from "expo-splash-screen";
+
+const useCachedResources = () => {
+    const [isLoadingComplete, setLoadingComplete] = useState(false);
+    useEffect(() => {
+        async function loadResourcesAndDataAsync() {
+            try {
+                await SplashScreen.preventAutoHideAsync();
+                const images = [require('../assets/splash.png')];
+                images.map(async image =>
+                    await Asset.fromModule(image).downloadAsync());
+            } catch (e) {console.warn(e);}
+             finally {
+                setLoadingComplete(true);
+                await SplashScreen.hideAsync();
+            }
+        }
+        loadResourcesAndDataAsync();
+    }, []);
+    return isLoadingComplete;
+}
 
 const App = () => {
-    return(
+    const isLoadingComplete = useCachedResources();
+    if (!isLoadingComplete) {
+        return null;
+    }
+    return (
         <ThemeProvider theme={theme}>
             <StatusBar barStyle="dark-content"/>
         </ThemeProvider>
