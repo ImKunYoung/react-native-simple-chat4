aabac2af35a7c886d9bfaf5c9135397954ec9bec
ImKunYoung <lim8667@gmail.com>
2022-07-03 17:48:02 +0900

    로그인 화면 - 이메일, 비밀번호 입력창

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 import Image from "./Image";
-export {Image}
+import Input from "./Input";
+
+export {Image, Input}
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import styled from 'styled-components/native';
 import {Button} from "react-native";
 import {images} from "../utils/images";
-import {Image} from "../components";
+import {Image, Input} from "../components";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${({theme}) => theme.background};
+  padding: 20px;
 
 `
 
 const Login = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef();
+
     const _handleSignup = () => {navigation.navigate('Signup')}
     return (
         <Container>
             <Image url={images.logo} imageStyle={{borderRadius:8}}/>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={text=>setEmail(text)}
+                onSubmitEditing={() => {}}
+                placeholder="Email"
+                returnKeyType="next"
+
+            />
+            <Input
+                label="Password"
+                value={password}
+                onChangeText={text=>setPassword(text)}
+                onSubmitEditing={() => {}}
+                placeholder="Password"
+                returnKeyType="done"
+                isPassword
+            />
             <Button title="Signup" onPress={_handleSignup}/>
         </Container>
     )
