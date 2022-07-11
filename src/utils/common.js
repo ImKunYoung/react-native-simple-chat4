55927c2ae78f3aa7614bda7d949f4deeaf55388b
ImKunYoung <lim8667@gmail.com>
2022-07-11 19:07:10 +0900

    Input 컴포넌트에 입력 받은 문자열의 공백 제거

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,6 @@
 /*입력 받은 값이 이메일 형식인지 확인*/
 export const validateEmail = email => {
-
+    /*이메일 형식*/
     const regex = /^[0-9?A-z0-9?]+(\.)?[0-9?A-z0-9?]+@[0-9?A-z]+\.[A-z]{2}.?[A-z]{0,3}$/
 
     /*검색된 문자열에 패턴이 있는지 여부 boolean*/
@@ -8,3 +8,11 @@ export const validateEmail = email => {
 
 }
 
+/*입력 받은 문자열의 공백 제거*/
+export const removeWhitespace = text => {
+    /* 문자열 내 모든 공백 */
+    const regex = /\s/g;
+
+    /*공백 제거된 문자열 반환*/
+    return text.replace(regex, '');
+}
