20743b3b9a18cad2d3d267116c30554cc936abba
ImKunYoung <lim8667@gmail.com>
2022-07-12 07:20:32 +0900

    모든 screen 헤더색 변경 - 버튼과 타이틀 색이 일치되도록

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -13,10 +13,12 @@ const AuthStack = () => {
             screenOptions={{
                 headerTitleAlign:'center',
                 cardStyle:{backgroundColor:theme.background},
-                headerTintColor: theme.headerTintColor,
+                headerTintColor: theme.headerTintColor, /*헤더색 변경 - 버튼과 타이틀 색이 일치되도록*/
             }}
         >
+            {/*헤더 가림*/}
             <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+            {/*헤더 뒤로가기 버튼에 타이틀 가림*/}
             <Stack.Screen name="Signup" component={Signup} options={{headerBackTitleVisible: false}}/>
         </Stack.Navigator>
 
