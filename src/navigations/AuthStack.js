a025fa14dd461d9f9bb12412ccd6ae4e984fbdb8
ImKunYoung <lim8667@gmail.com>
2022-07-12 07:13:25 +0900

    로그인 screen 헤더 감춤

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -15,7 +15,7 @@ const AuthStack = () => {
                 cardStyle:{backgroundColor:theme.background},
             }}
         >
-            <Stack.Screen name="Login" component={Login}/>
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
             <Stack.Screen name="Signup" component={Signup}/>
         </Stack.Navigator>
 
