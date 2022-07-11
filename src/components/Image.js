4677640ec73a39278c2491967f4d0e6cffeed1c8
ImKunYoung <lim8667@gmail.com>
2022-07-12 07:36:17 +0900

    회원 가입 화면(Signup Screen) - Image 원형으로 렌더링되게

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
+import PropTypes from "prop-types";
 
 const Container = styled.View`
   align-self: center;
@@ -10,14 +11,25 @@ const StyledImage = styled.Image`
   width: 100px;
   height: 100px;
   background-color: ${({theme})=> theme.imageBackground};
+  border-radius: ${({rounded}) => (rounded ? 50 : 0)};
 `;
 
-const Image = ({url, imageStyle}) => {
+const Image = ({url, imageStyle, rounded}) => {
     return (
         <Container>
-            <StyledImage source={{uri:url}} style={imageStyle}/>
+            <StyledImage source={{uri:url}} style={imageStyle} rounded={rounded}/>
         </Container>
     )
 
 }
 export default Image
+
+Image.defaultProps = {
+    rounded: false,
+}
+
+Image.propTypes = {
+    url: PropTypes.string,
+    imageStyle: PropTypes.object,
+    rounded: PropTypes.bool,
+}
