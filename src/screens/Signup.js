44cbb66e73bdb279693e77fc8a04079719476ed2
ImKunYoung <lim8667@gmail.com>
2022-07-12 08:36:29 +0900

    re

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,20 +1,62 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components/native";
 import {Text} from "react-native";
+import {validateEmail} from "../utils/common";
+import PropTypes from "prop-types";
+import Button from "../components/Button";
 
 const Container = styled.View`
-    flex: 1;
+  flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${({theme}) => theme.background};
 
 `;
+const ErrorText = styled.Text`
 
-const Signup = () => {
+`
+export const ErrorMessage = ({names, email, password, passwordConfirm}) => {
+    let _errorMessage = ' ';
+    if(!names) {
+        _errorMessage = 'Please verify your name.'
+    } else if (!validateEmail(email)) {
+        _errorMessage = 'Please verify your email.'
+    } else if (password.length < 6) {
+        _errorMessage = 'The password must contain 6 characters at least.'
+    } else if (password !== passwordConfirm) {
+        _errorMessage = 'Passwords need to match.'
+    } else {
+        _errorMessage = ' '
+    }
+    return _errorMessage
+}
+
+ErrorMessage.propTypes = {
+    password: PropTypes.string,
+}
+
+export const Signup = () => {
+    /*이름*/
+    const [names, setName] = useState('')
+    /*이메일*/
+    const [email, setEmail] = useState('')
+    /*비밀번호*/
+    const [password, setPassword] = useState('')
+    /*비밀번호 확인*/
+    const [passwordConfirm, setPasswordConfirm] = useState('')
+    /*에러 메시지*/
+    const [errorMessage, setErrorMessage] = useState('')
+    /*버튼 Disabled*/
+    const [disabled, setDisabled] = useState(true)
+
+    useEffect(() => {
+        return ErrorMessage(names, email, password, passwordConfirm);
+    }, [names, email, password, passwordConfirm]);
+
+
     return(
         <Container>
             <Text style={{fontSize: 30}}>Signup Screen</Text>
         </Container>
     )
 }
-export default Signup
--- test/utils/common.test.js
+++ test/utils/common.test.js
@@ -1,10 +1,12 @@
 const common = require('../../src/utils/common');
 
 test('Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인(이메일 형식인 경우)', () => {
+    console.log(common.validateEmail('ur2kunyoung2'))
     expect(common.validateEmail('ur2kunyoung2@outlook.com')).toBe(true);
 })
 
 test('Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인(이메일 형식이 아닌 경우)', () => {
+    console.log(common.validateEmail('ur2kunyoung2'))
     expect(common.validateEmail('ur2kunyoung2')).toBe(false);
 })
 
