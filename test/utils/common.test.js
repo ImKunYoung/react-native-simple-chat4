432f04be4f9a291e13817deed409b014c8ce3c71
ImKunYoung <lim8667@gmail.com>
2022-07-11 19:11:52 +0900

    Input 컴포넌트에 입력 받은 문자열의 공백 제거 - 테스트

--- test/utils/common.test.js
+++ test/utils/common.test.js
@@ -1,11 +1,15 @@
 const common = require('../../src/utils/common');
 
-/*Test1 - 테스트Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인한다 (이메일 형식인 경우)*/
-test('emailValidation', () => {
+test('Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인(이메일 형식인 경우)', () => {
     expect(common.validateEmail('ur2kunyoung2@outlook.com')).toBe(true);
 })
 
-/*Test2 - 테스트Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인한다 (이메일 형식이 아닌 경우)*/
-test('emailValidation', () => {
+test('Input 컴포넌트에 입력 받은 값이 이메일 형식인지 확인(이메일 형식이 아닌 경우)', () => {
     expect(common.validateEmail('ur2kunyoung2')).toBe(false);
+})
+
+test('Input 컴포넌트에 입력 받은 문자열의 공백 제거', () => {
+    const before = '가 나다라 마 '
+    const after = '가나다라마'
+    expect(common.removeWhitespace(before)).toEqual(after)
 })
