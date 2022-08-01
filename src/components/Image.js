7b65dded6c9149a23162f37a441740dd956d2695
ImKunYoung <lim8667@gmail.com>
2022-08-01 15:17:35 +0900

    image 컴포넌트에 버튼 추가

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,18 +1,35 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
+import {MaterialIcons} from "@expo/vector-icons";
 
 const Container = styled.View`
   align-self: center;
   margin-bottom: 30px;
-
 `;
+
 const StyledImage = styled.Image`
   width: 100px;
   height: 100px;
   background-color: ${({theme})=> theme.imageBackground};
   border-radius: ${({rounded}) => (rounded ? 50 : 0)}px;
 `;
+
+const ButtonContainer = styled.TouchableOpacity`
+
+`;
+
+const ButtonIcon = styled(MaterialIcons)`
+`;
+
+const PhotoButton = ({onPress}) => {
+    return (
+        <ButtonContainer onPress={onPress}>
+            <ButtonIcon/>
+        </ButtonContainer>
+    )
+}
+
 
 const Image = ({url, imageStyle, rounded}) => {
     return (
